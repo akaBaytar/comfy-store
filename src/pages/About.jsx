4704ed33ccc04d24674dcb5961ef69c41e8ff50b
Burak Bilgili <burak.bilgili@icloud.com>
2024-01-18 22:51:23 +0300

    about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,36 @@
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center'>
+      <h1 className='text-4xl font-bold leading-none tracking-tight sm:text-6xl'>
+        Elevating
+      </h1>
+      <div className='stats bg-primary shadow'>
+        <div className='stat'>
+          <div className='stat-title text-primary-content text-4xl sm:text-6xl font-bold tracking-widest'>
+            comfy
+          </div>
+        </div>
+      </div>
+      <p className='mt-6 text-md leading-8 max-w-2xl mx-auto'>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+        assumenda nostrum deserunt sequi, ipsa similique maiores sunt cupiditate
+        ut perferendis sit voluptate laboriosam est suscipit dolorum? Vero sed
+        voluptatum officia.
+      </p>
+      <p className='mt-6 text-md leading-8 max-w-2xl mx-auto'>
+        Beatae a nam eum soluta. Debitis facilis iste suscipit laborum rem
+        itaque aliquid doloribus ab deleniti eveniet, porro quis molestias
+        voluptates repudiandae vitae sint sapiente?
+      </p>
+      <p className='mt-6 text-md leading-8 max-w-2xl mx-auto'>
+        Ipsum officiis quo corrupti soluta, sapiente sed deleniti quas?
+        Distinctio reprehenderit consequatur facilis, magni voluptates voluptas
+        debitis, deleniti quasi possimus ullam minus facere voluptatibus aperiam
+        laudantium quam aspernatur ut libero provident? Repellendus accusamus
+        minus quod?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
