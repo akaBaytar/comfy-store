f9e65bcb8b4de7f8609b09167ea2961bb9d8c2b4
Burak Bilgili <burak.bilgili@icloud.com>
2024-01-18 15:21:04 +0300

    pages and fundamental router logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,69 @@
-import { Fragment } from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  Home,
+  Landing,
+  Login,
+  Orders,
+  Product,
+  Products,
+  Register,
+} from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'products',
+        element: <Products />,
+      },
+      {
+        path: 'products/:id',
+        element: <Product />,
+      },
+      {
+        path: 'cart',
+        element: <Cart />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'checkout',
+        element: <Checkout />,
+      },
+      {
+        path: 'orders',
+        element: <Orders />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+    errorElement: <Error />,
+  },
+]);
 
 const App = () => {
-  return <Fragment></Fragment>;
+  return <RouterProvider router={router}></RouterProvider>;
 };
 
 export default App;
