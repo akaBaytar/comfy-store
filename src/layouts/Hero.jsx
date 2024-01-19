f86cafc9c98b679d60f17244034c9b3956a1595a
Burak Bilgili <burak.bilgili@icloud.com>
2024-01-19 15:47:30 +0300

    hero section adjusments

--- src/layouts/Hero.jsx
+++ src/layouts/Hero.jsx
@@ -4,11 +4,11 @@ import Carousel from '../components/Carousel';
 const Hero = () => {
   return (
     <section className='grid lg:grid-cols-2 items-center gap-y-20'>
-      <div className='text-center lg:text-start'>
+      <div className='text-center lg:text-start mx-auto'>
         <h1 className='max-w-2xl text-4xl sm:text-6xl font-bold tracking-tight'>
           We are changing the way people shop
         </h1>
-        <p className='mt-8 max-w-2xl text-lg sm:text-xl leading-8'>
+        <p className='mx-auto mt-8 max-w-xl text-lg sm:text-xl leading-8'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
           praesentium, incidunt explicabo inventore vero voluptates minus
           provident labore laudantium quae.
@@ -19,7 +19,7 @@ const Hero = () => {
           </Link>
         </div>
       </div>
-      <div className='h-[30rem] mx-auto lg:mx-0 max-w-sm sm:max-w-lg md:max-w-2xl lg:max-w-96 p-4 bg-neutral rounded-box lg:justify-self-end'>
+      <div className='h-[30rem] lg:mx-0 min-w-full max-w-sm sm:max-w-lg md:max-w-2xl lg:max-w-96 p-4 bg-neutral rounded-box lg:justify-self-end'>
         <Carousel />
       </div>
     </section>
