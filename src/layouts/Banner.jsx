78363ef9668c98a0d7e0d6cd5fe4b62074752f7a
Burak Bilgili <burak.bilgili@icloud.com>
2024-01-20 17:32:44 +0300

    banner section adjusments

--- src/layouts/Banner.jsx
+++ src/layouts/Banner.jsx
@@ -15,7 +15,7 @@ const Banner = () => {
             SHOP NOW
           </Link>
         </article>
-        <article className='rounded-2xl h-60 pl-4 pt-4 text-base-300 sm:row-start-1'>
+        <article className='rounded-2xl h-60 pl-4 pt-4 text-black sm:row-start-1'>
           <h4 className='text-xl tracking-widest lg:text-3xl'>Up to 30% off</h4>
           <h3 className='text-xl tracking-widest lg:text-3xl mb-3'>
             Top lamp brands
@@ -26,7 +26,7 @@ const Banner = () => {
             SHOP NOW
           </Link>
         </article>
-        <article className='rounded-2xl h-60 pl-4 pt-4 text-base-300 sm:row-start-2'>
+        <article className='rounded-2xl h-60 pl-4 pt-4 text-black sm:row-start-2'>
           <h4 className='text-xs font-bold'>New Products</h4>
           <h3 className='text-xl lg:text-3xl font-bold mt-2 mb-4 tracking-wide'>
             Up to 25% off cabinet
@@ -37,7 +37,7 @@ const Banner = () => {
             SHOP NOW
           </Link>
         </article>
-        <article className='rounded-2xl h-64 text-center sm:text-start sm:flex sm:items-center sm:gap-8 sm:pl-8 text-base-300 sm:col-span-2 sm:row-span-1 sm:h-40'>
+        <article className='rounded-2xl h-64 text-center sm:text-start sm:flex sm:items-center sm:gap-8 sm:pl-8 text-black sm:col-span-2 sm:row-span-1 sm:h-40'>
           <h4 className='mt-4 font-bold text-5xl sm:text-6xl'>10%</h4>
           <div className='sm:mt-3'>
             <h3 className='text-xl tracking-wide mt-2 sm:text-3xl sm:tracking-widest'>
