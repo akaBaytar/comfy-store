2522c10b0025b9d69d6fb7912983ef416dbac52c
Burak Bilgili <burak.bilgili@icloud.com>
2024-01-20 16:25:25 +0300

    categories section adjusment

--- src/layouts/Categories.jsx
+++ src/layouts/Categories.jsx
@@ -7,19 +7,15 @@ const Categories = () => {
   return (
     <section className='pt-20'>
       <Title text={'all categories'} />
-      <div className='pt-8 grid gap-8 grid-cols-2 lg:grid-cols-3'>
-        {categories.map(({ id, img, text }) => (
-          <Link
-            key={id}
-            to='/products'
-            className='card card-compact shadow-lg group overflow-hidden'>
-            <img
-              src={img}
-              alt={text}
-              className='rounded-t-xl h-32 object-contain group-hover:scale-110 duration-200'
-            />
-            <div className='card-body items-center text-center'>
-              <h2 className='card-title'>{text}</h2>
+      <div className='pt-8 grid gap-8 grid-cols-2 md:grid-cols-3'>
+        {categories.map(({ id, img, text, count }) => (
+          <Link key={id} to='/products' className='py-1 flex items-center gap-4 lg:gap-8 group'>
+            <div className='bg-base-300 rounded-full w-24 h-24 md:w-28 md:h-28 grid place-items-center'>
+              <img src={img} alt={text} className='h-20 md:h-24 object-cover group-hover:scale-125 duration-200' />
+            </div>
+            <div className=''>
+              <h2 className='font-bold text-lg md:text-xl'>{text}</h2>
+              <span className='text-xs md:text-sm text-secondary'>{`${count} products`}</span>
             </div>
           </Link>
         ))}
--- src/util/index.js
+++ src/util/index.js
@@ -19,12 +19,12 @@ import category5 from '../assets/categories/tables.png';
 import category6 from '../assets/categories/cabinets.png';
 
 export const categories = [
-  { id: 1, img: category1, text: 'Beds' },
-  { id: 2, img: category2, text: 'Chairs' },
-  { id: 3, img: category3, text: 'Kids' },
-  { id: 4, img: category4, text: 'Sofas' },
-  { id: 5, img: category5, text: 'Tables' },
-  { id: 6, img: category6, text: 'Cabinets' },
+  { id: 1, img: category1, text: 'Beds', count: 5 },
+  { id: 2, img: category2, text: 'Chairs', count: 8 },
+  { id: 3, img: category3, text: 'Kids', count: 9 },
+  { id: 4, img: category4, text: 'Sofas', count: 7 },
+  { id: 5, img: category5, text: 'Tables', count: 4 },
+  { id: 6, img: category6, text: 'Cabinets', count: 3 },
 ];
 
 // base URL
