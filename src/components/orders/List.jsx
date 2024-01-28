102d2d6287552ec71cbebe565ec9129dc0b74572
Burak Bilgili <burak.bilgili@icloud.com>
2024-01-28 18:32:09 +0300

    render orders in table and formatted date (dayjs plugin)

--- src/components/orders/List.jsx
+++ src/components/orders/List.jsx
@@ -1,7 +1,51 @@
-const List = () => {
-  return (
-    <div>List</div>
-  )
-}
+import { useLoaderData } from 'react-router-dom';
 
-export default List
+import day from 'dayjs';
+import advancedFormat from 'dayjs/plugin/advancedFormat';
+
+day.extend(advancedFormat);
+
+const List = () => {
+  const { orders, meta } = useLoaderData();
+
+  return (
+    <div className='mt-4'>
+      <h4 className='mb-4'>Total Orders: {meta.pagination.total}</h4>
+      <div className='overflow-x-auto'>
+        <table className='table table-zebra'>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Address</th>
+              <th>Products</th>
+              <th>Cost</th>
+              <th className='hidden sm:block'>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => {
+              const id = order.id;
+
+              const { name, address, numItemsInCart, orderTotal, createdAt } =
+                order.attributes;
+
+              const date = day(createdAt).format('hh:mm a - MMM Do, YYYY');
+
+              return (
+                <tr key={id}>
+                  <td>{name}</td>
+                  <td>{address}</td>
+                  <td>{numItemsInCart}</td>
+                  <td>{orderTotal}</td>
+                  <td>{date}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default List;
